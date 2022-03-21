d35e592b6d1a415bbb56764120e0dce4878adaee
bahtiarhendra <97088446+bahtiarhendra@users.noreply.github.com>
2022-03-21 22:44:50 +0700

    Update kalkulat.cpp

--- PRAKTIKUM 1/Hendra/kalkulat.cpp
+++ PRAKTIKUM 1/Hendra/kalkulat.cpp
@@ -4,4 +4,35 @@ using namespace std;
 int main(){
     cout<<"UNIVERSITAS AHMAD DAHLAN";
     cout<<"PRAKTIKUM ALPRO JUMAT 13.30";
+    float a,b,hasil;
+	char kalkulator;
+	
+	cout<<"SELAMAT DATANG DI PROGRAM KALKULATOR\n"<<endl;
+	cout<<"MASUKAN ANGKA PERTAMA = ";
+	cin>>a;
+	cout<<"PILIH OPERATOR +,-,/,* : ";
+	cin>>kalkulator;
+	cout<<"MASUKAN ANGKA KEDUA = ";
+	cin>>b;
+	cout<<"Hasil Perhitungan ";
+	cout<< a << kalkulator << b;
+	
+	if(kalkulator=='+'){
+		hasil = a+b;
+	}
+	else if(kalkulator=='-'){
+		hasil = a-b;
+	}
+	else if(kalkulator=='/'){
+		hasil = a/b;
+	}
+	else if(kalkulator=='*'){
+		hasil = a*b;
+	}
+	else{
+		cout<<"operator anda salah "<<endl;
+	}
+	cout<<" = "<< hasil <<endl;
+	
+	return 0;
 }
